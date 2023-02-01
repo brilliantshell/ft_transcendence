872a1e7834740ca45c2d77c0eb0b0555e1fdbd2c
42ghan <ghan@student.42seoul.kr>
2023-02-01 13:19:56 +0900

    :hammer: chore : TypeOrmModule.forFeature import

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserGateway } from './user.gateway';
 import { UserService } from './user.service';
 import { UserStatusModule } from '../user-status/user-status.module';
+import { Users } from '../entity/users.entity';
 
 @Module({
-  imports: [UserStatusModule],
-  providers: [UserGateway],
-  // providers: [UserGateway, UserService],
+  imports: [TypeOrmModule.forFeature([Users]), UserStatusModule],
+  providers: [UserGateway, UserService],
 })
 export class UserModule {}
