e35ac645809bcd85ebc76ac58702b62c2e909862
hankkim <hkkim0103@naver.com>
2023-02-02 18:24:26 +0900

    💄 style : layout 구성 #83 layout을 grid를 활용하여 구성

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import '../App.css';
+import './Sidebar.css';
+
+function Sidebar() {
+	return (
+		<nav>
+			Navigation bar
+			{/* <button className="btn"> Button </button>
+			<button className="btn"> Button </button>
+			<button className="btn"> Button </button>
+			<button className="btn"> Button </button>
+			<button>logout</button> */}
+		</nav>
+	);
+}
+
+export default Sidebar
