91e2f2d3935fa533193f4247b90595ba15f73bed
hankkim <hkkim0103@naver.com>
2023-02-04 12:19:56 +0900

    ✨ feat : react-router-dom 적용 #92

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,38 @@
+import React from 'react';
+import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom';
+import Ranks from './components/Ranks';
+import Profile from './components/Profile';
 import './App.css';
-import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="aaa">
+    <BrowserRouter>
       <header>headerheaderheaderheaderheader</header>
-      <Sidebar />
-      <main>mainmainmainmainmainmainmainmain</main>
-    </div>
+      <nav>
+        {/* 위에 로고 */}
+        <ul>
+          <li>
+            <NavLink to="/profile">Profile</NavLink>
+          </li>
+          <li>
+            <NavLink to="/ranks">Ranks</NavLink>
+          </li>
+          <li>
+            <NavLink to="/chats">Chats</NavLink>
+          </li>
+          <li>
+            <NavLink to="/game">Game</NavLink>
+          </li>
+        </ul>
+        {/* 아래 로그아웃 */}
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/ranks" element={<Ranks />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
