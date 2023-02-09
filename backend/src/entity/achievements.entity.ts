cec81e05de34b363317b221514b1cf3668379e58
yongjulejule <lyjshow200@gmail.com>
2023-02-09 22:20:26 +0900

    :recycle: refactor : achievements 에서 imagePath 삭제

--- backend/src/entity/achievements.entity.ts
+++ backend/src/entity/achievements.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-@Index(['achievementId', 'title', 'about', 'imagePath'], { unique: true })
+@Index(['achievementId', 'title', 'about'], { unique: true })
 export class Achievements {
   @PrimaryGeneratedColumn({
     primaryKeyConstraintName: 'achievements_pkey',
@@ -14,7 +14,4 @@ export class Achievements {
 
   @Column({ type: 'varchar', length: 256, nullable: false })
   about: string;
-
-  @Column({ type: 'varchar', length: 256, name: 'image_path', nullable: false })
-  imagePath: string;
 }
