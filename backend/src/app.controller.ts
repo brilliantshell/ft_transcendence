83bf5e27322b4a02c65940654d87f20c4af2f9a3
yongjulejule <lyjshow200@gmail.com>
2023-02-24 15:31:46 +0900

    :sparkles: feat : search 기능 draft

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Delete, Get, HttpStatus, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Response } from 'express';
+import { Like, Repository } from 'typeorm';
 
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
+import { Users } from './entity/users.entity';
 import { VerifiedRequest } from './util/type';
 
 @Controller()
@@ -10,6 +21,8 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly authService: AuthService,
+    @InjectRepository(Users)
+    private readonly usersRepository: Repository<Users>,
   ) {}
 
   @Get()
@@ -25,5 +38,16 @@ export class AppController {
       .cookie('accessToken', '', { expires: new Date(0) })
       .cookie('refreshToken', '', { expires: new Date(0) })
       .end();
+  }
+
+  @Get('/search')
+  search(@Req() req: VerifiedRequest, @Query('search') search: string) {
+    return this.usersRepository.find({
+      where: {
+        nickname: Like(`${search}%`),
+      },
+      take: 10,
+      select: ['userId', 'nickname', 'isDefaultImage'],
+    });
   }
 }
