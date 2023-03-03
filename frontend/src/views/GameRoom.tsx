50f0e527c387cdb303443028bc56124655afc13e
42ghan <ghan@student.42seoul.kr>
2023-03-04 01:08:41 +0900

    :sparkles: feat : 게임 UI render 시 관전자와 플레이어 케이스 분리 #197

--- frontend/src/views/GameRoom.tsx
+++ frontend/src/views/GameRoom.tsx
@@ -2,7 +2,7 @@ import GameInfo from '../components/GameRoom/GameInfo';
 import GameMenu from '../components/GameRoom/GameMenu';
 import GamePlay from '../components/GameRoom/GamePlay';
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { socket } from '../util/Socket';
 import { useCurrentUi } from '../components/hooks/EmitCurrentUi';
 import {
@@ -13,16 +13,19 @@ import '../style/GameRoom.css';
 
 export default function GameRoom() {
   const { gameId } = useParams();
+  const location = useLocation();
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [isStarted, setIsStarted] = useState(false);
+  const isSpectator = location.state?.isSpectator ?? false;
 
   useCurrentUi(isConnected, setIsConnected, `game-${gameId}`);
   const { gameInfo, players } = useRequestGame(
     isConnected,
+    isSpectator,
     gameId ?? 'example',
     setIsStarted,
   );
-  useListenGameEvents(isConnected);
+  useListenGameEvents(isConnected, isSpectator);
 
   return (
     <div className="gameRoom">
