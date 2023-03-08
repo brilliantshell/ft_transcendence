0384a238ef864d197147ffebb60f18956095d3d6
42ghan <ghan@student.42seoul.kr>
2023-03-08 21:33:09 +0900

    :sparkles: feat : 수동적으로 반응형 게임 그리는 GamePainter #212 #209

--- frontend/src/components/GameRoom/hooks/GamePlayHooks.tsx
+++ frontend/src/components/GameRoom/hooks/GamePlayHooks.tsx
@@ -1,5 +1,6 @@
 import { ControllerType, Dimensions, GameInfo } from '../util/interfaces';
 import { GamePainter } from '../util/GamePainter';
+import { socket } from '../../../util/Socket';
 import { useEffect } from 'react';
 
 export function useGamePlay(
@@ -9,6 +10,24 @@ export function useGamePlay(
   dimensions: Dimensions,
 ) {
   useEffect(() => {
+    const keyDownHandler = (e: KeyboardEvent) => {
+      if (e.key === 'Up' || e.key === 'ArrowUp') {
+        e.preventDefault();
+        socket.emit('gamePlayerY', {
+          gameId: gameInfo.id,
+          isLeft: controllerType.isLeft,
+          isUp: true,
+        });
+      } else if (e.key === 'Down' || e.key === 'ArrowDown') {
+        e.preventDefault();
+        socket.emit('gamePlayerY', {
+          gameId: gameInfo.id,
+          isLeft: controllerType.isLeft,
+          isUp: false,
+        });
+      }
+    };
+
     const canvas = canvasRef.current;
     let painter: GamePainter;
     if (canvas) {
@@ -21,32 +40,16 @@ export function useGamePlay(
           dimensions,
         );
         if (controllerType.isPlayer) {
-          document.addEventListener(
-            'keyup',
-            painter.keyUpHandler.bind(painter),
-          );
-          document.addEventListener(
-            'keydown',
-            painter.keyDownHandler.bind(painter),
-          );
-          painter.startGame();
-        } else {
-          painter.spectateGame();
+          document.addEventListener('keydown', keyDownHandler);
         }
+        painter.drawGame();
       }
     }
     return () => {
       if (painter !== undefined) {
-        clearInterval(painter.intervalId);
+        socket.off('gameData');
         if (controllerType.isPlayer) {
-          document.removeEventListener(
-            'keyup',
-            painter.keyUpHandler.bind(painter),
-          );
-          document.removeEventListener(
-            'keydown',
-            painter.keyDownHandler.bind(painter),
-          );
+          document.removeEventListener('keydown', keyDownHandler);
         }
       }
     };
--- frontend/src/components/GameRoom/util/GamePainter.ts
+++ frontend/src/components/GameRoom/util/GamePainter.ts
@@ -1,23 +1,16 @@
 import {
-  BallData,
+  BallCoordinates,
   ControllerType,
+  GameDataMessage,
   GameInfo,
   GameMetaData,
-  KeyPressed,
   PaddlePositions,
+  Score,
 } from './interfaces';
-import { Subject } from 'rxjs';
-import { listenEvent } from '../../../util/Socket';
 import { socket } from '../../../util/Socket';
 
 export class GamePainter {
-  intervalId: number = 0;
-  private ballData: BallData;
-  private metaData: GameMetaData;
-  private readonly keyPressed: KeyPressed = { up: false, down: false };
-  private paddlePositions: PaddlePositions;
-  private scores: [number, number] = [0, 0];
-  private readonly subject: Subject<void> = new Subject();
+  private readonly metaData: GameMetaData;
 
   constructor(
     private readonly context: CanvasRenderingContext2D,
@@ -27,12 +20,6 @@ export class GamePainter {
   ) {
     this.context.font = '4rem DungGeunMo';
     this.metaData = new GameMetaData(dimension);
-    const { paddleW } = this.metaData;
-    this.ballData = this.resetBallData({ xDirection: 1, yDirection: 1 });
-    this.paddlePositions = {
-      myY: (dimension.h - paddleW) / 2,
-      opponentY: (dimension.h - paddleW) / 2,
-    };
   }
 
   /*****************************************************************************
@@ -41,81 +28,24 @@ export class GamePainter {
    *                                                                           *
    ****************************************************************************/
 
-  spectateGame() {
-    socket.on('gameSpectate', ({ ballData, paddlePositions, scores }) => {
-      if (ballData === null) {
-        this.drawResult();
-        socket.off('gameSpectate');
-        return;
-      }
-      this.ballData = ballData;
-      this.paddlePositions = paddlePositions;
-      this.scores = scores;
-      this.context.clearRect(0, 0, this.dimension.w, this.dimension.h);
-      this.drawLine();
-      this.drawBall();
-      this.drawPaddles();
-      this.drawScores();
-    });
-  }
-
-  async startGame() {
-    const [directions] = await Promise.all([
-      listenEvent<{
-        xDirection: 1 | -1;
-        yDirection: 1 | -1;
-      }>('gameBallDirections'),
-      socket.emit('gameResetBall', { gameId: this.gameInfo.id }),
-    ]);
-    this.ballData = this.resetBallData(directions);
-    socket.off('gameBallDirections');
-    this.subject.subscribe(() => {
-      if (this.controllerType.isLeft) {
-        socket.emit('gameData', {
-          gameId: this.gameInfo.id,
-          ballData: this.ballData,
-          paddlePositions: this.paddlePositions,
-          scores: this.scores,
-        });
-      }
-      this.context.clearRect(0, 0, this.dimension.w, this.dimension.h);
-      this.drawLine();
-      this.drawBall();
-      this.drawPaddles();
-      this.drawScores();
-      const { x, dx } = this.ballData;
-      const { boardEdges } = this.metaData;
-      if (x + dx < boardEdges.left || x + dx > boardEdges.right) {
-        this.updateScore();
-      } else {
-        this.updateBallData();
-        this.updatePaddlePosition();
-      }
-    });
-    socket.on('gameOpponentY', ({ y }: { isLeft: boolean; y: number }) => {
-      this.paddlePositions.opponentY = y;
-    });
-    this.intervalId = setInterval(() => this.subject.next(), 10);
-  }
-
-  keyDownHandler(e: KeyboardEvent) {
-    if (e.key === 'Up' || e.key === 'ArrowUp') {
-      e.preventDefault();
-      this.keyPressed.up = true;
-    } else if (e.key === 'Down' || e.key === 'ArrowDown') {
-      e.preventDefault();
-      this.keyPressed.down = true;
-    }
-  }
-
-  keyUpHandler(e: KeyboardEvent) {
-    if (e.key === 'Up' || e.key === 'ArrowUp') {
-      e.preventDefault();
-      this.keyPressed.up = false;
-    } else if (e.key === 'Down' || e.key === 'ArrowDown') {
-      e.preventDefault();
-      this.keyPressed.down = false;
-    }
+  drawGame() {
+    socket.on(
+      'gameData',
+      ({ ballCoords, paddlePositions, scores }: GameDataMessage) => {
+        this.context.clearRect(0, 0, this.dimension.w, this.dimension.h);
+        this.drawLine();
+        this.drawBall(ballCoords);
+        this.drawPaddles(paddlePositions);
+        this.drawScores(scores);
+      },
+    );
+    socket.once(
+      'gameComplete',
+      ({ winnerSide }: { winnerSide: 'left' | 'right' }) => {
+        socket.off('gameData');
+        this.drawResult(winnerSide);
+      },
+    );
   }
 
   /*****************************************************************************
@@ -124,89 +54,8 @@ export class GamePainter {
    *                                                                           *
    ****************************************************************************/
 
-  private updateScore() {
-    const { midX } = this.metaData;
-    this.scores[this.ballData.x > midX ? 0 : 1] += 1;
-    clearInterval(this.intervalId);
-    setTimeout(() => {
-      if (this.scores[0] === 5 || this.scores[1] === 5) {
-        this.finishGame();
-      } else {
-        Promise.all([
-          listenEvent<{ xDirection: 1 | -1; yDirection: 1 | -1 }>(
-            'gameBallDirections',
-          ).then(directions => {
-            socket.off('gameBallDirections');
-            this.ballData = this.resetBallData(directions);
-            this.intervalId = setInterval(() => this.subject.next(), 10);
-          }),
-          socket.emit('gameResetBall', { gameId: this.gameInfo.id }),
-        ]);
-      }
-    }, 250);
-  }
-
-  private updatePaddlePosition() {
-    const { myY } = this.paddlePositions;
-    const { paddleW } = this.metaData;
-    if (this.keyPressed.up && myY > 0) {
-      this.paddlePositions.myY = Math.max(myY - 8, 0);
-      socket.emit('gamePlayerY', {
-        gameId: this.gameInfo.id,
-        y: this.paddlePositions.myY,
-      });
-    } else if (this.keyPressed.down && myY < this.dimension.h - paddleW) {
-      this.paddlePositions.myY = Math.min(myY + 8, this.dimension.h - paddleW);
-      socket.emit('gamePlayerY', {
-        gameId: this.gameInfo.id,
-        y: this.paddlePositions.myY,
-      });
-    }
-  }
-
-  private updateBallData() {
-    let { x, y, dx, dy } = this.ballData;
-    const { boardEdges, paddleEnds, paddleW } = this.metaData;
-    const { myY, opponentY } = this.paddlePositions;
-    const { isPlayer, isLeft } = this.controllerType;
-    const nextX = x + dx;
-    const nextY = y + dy;
-    if (nextY < boardEdges.top || nextY > boardEdges.bottom) {
-      this.ballData.dy = -dy;
-    }
-    const [leftY, rightY] =
-      !isPlayer || isLeft ? [myY, opponentY] : [opponentY, myY];
-    const isPaddleTouched =
-      (x > paddleEnds.left &&
-        nextX < paddleEnds.left &&
-        nextY > leftY &&
-        nextY < leftY + paddleW) ||
-      (x < paddleEnds.right &&
-        nextX > paddleEnds.right &&
-        nextY > rightY &&
-        nextY < rightY + paddleW);
-    if (isPaddleTouched) {
-      this.ballData.dx = -dx;
-      this.accelerate();
-    }
-    this.ballData.x += this.ballData.dx;
-    this.ballData.y += this.ballData.dy;
-  }
-
-  private finishGame() {
-    socket.off('gameOpponentY');
-    const { isPlayer, isLeft } = this.controllerType;
-    if (isPlayer && (isLeft ? this.scores[0] : this.scores[1]) === 5) {
-      socket.emit('gameComplete', {
-        id: this.gameInfo.id,
-        scores: this.scores,
-      });
-    }
-    this.drawResult();
-  }
-
   private drawLine() {
-    const { midX } = this.metaData;
+    const midX = this.dimension.w / 2;
     this.context.beginPath();
     this.context.moveTo(midX, 0);
     this.context.setLineDash([10, 15]);
@@ -214,31 +63,31 @@ export class GamePainter {
     this.context.stroke();
   }
 
-  private drawBall() {
-    const { x, y } = this.ballData;
+  private drawBall(ballCoords: BallCoordinates) {
+    const { x, y } = ballCoords;
+    const { w, h } = this.dimension;
     this.context.beginPath();
-    this.context.arc(x, y, this.metaData.radius, 0, Math.PI * 2);
+    this.context.arc(x * w, y * h, w / 120, 0, Math.PI * 2);
     this.context.fillStyle = '#ccadac';
     this.context.fill();
     this.context.closePath();
   }
 
-  private drawPaddles() {
-    const { paddleW, paddleH, radius } = this.metaData;
-    const { myY, opponentY } = this.paddlePositions;
-    const { isPlayer, isLeft } = this.controllerType;
-    const yCoords = !isPlayer || isLeft ? [myY, opponentY] : [opponentY, myY];
+  private drawPaddles(paddlePositions: PaddlePositions) {
+    const { h } = this.dimension;
+    const { radius, paddleW, paddleH } = this.metaData;
+    const { leftY, rightY } = paddlePositions;
     this.context.beginPath();
     this.context.roundRect(
       radius * 2,
-      yCoords[0],
+      leftY * h,
       paddleH,
       paddleW,
       paddleH / 2,
     );
     this.context.roundRect(
       this.dimension.w - paddleH - radius * 2,
-      yCoords[1],
+      rightY * h,
       paddleH,
       paddleW,
       paddleH / 2,
@@ -248,9 +97,9 @@ export class GamePainter {
     this.context.closePath();
   }
 
-  private drawScores() {
+  private drawScores(scores: [Score, Score]) {
     this.context.fillStyle = '#ccadac';
-    const scoresString = `${this.scores[0]}        ${this.scores[1]}`;
+    const scoresString = `${scores[0]}        ${scores[1]}`;
     this.context.fillText(
       scoresString,
       this.metaData.midX - this.context.measureText(scoresString).width / 2,
@@ -258,48 +107,21 @@ export class GamePainter {
     );
   }
 
-  private drawResult() {
+  private drawResult(winnerSide: 'left' | 'right') {
     const { isPlayer, isLeft } = this.controllerType;
     this.context.clearRect(0, 0, this.dimension.w, this.dimension.h);
     this.context.fillStyle = '#ccadac';
     let resultString;
+    const messages = isLeft
+      ? ['You won!', 'You lost...']
+      : ['You lost...', 'You won!'];
     resultString = isPlayer
-      ? this.scores[isLeft ? 0 : 1] === 5
-        ? 'You won!'
-        : 'You lost...'
-      : `${
-          this.gameInfo.players[this.scores[0] > this.scores[1] ? 0 : 1]
-        } won!`;
+      ? messages[winnerSide === 'left' ? 0 : 1]
+      : `${this.gameInfo.players[winnerSide === 'left' ? 0 : 1]} won!`;
     this.context.fillText(
       resultString,
       this.metaData.midX - this.context.measureText(resultString).width / 2,
       this.metaData.midY,
     );
-  }
-
-  private accelerate() {
-    let { dx, dy } = this.ballData;
-    const { maxSpeed, acceleration } = this.metaData;
-    if (dx < maxSpeed) {
-      this.ballData.dx += dx > 0 ? acceleration : -acceleration;
-    }
-    if (dy < maxSpeed) {
-      this.ballData.dy += dy > 0 ? acceleration : -acceleration;
-    }
-  }
-
-  private resetBallData({
-    xDirection,
-    yDirection,
-  }: {
-    xDirection: 1 | -1;
-    yDirection: 1 | -1;
-  }) {
-    return {
-      x: this.metaData.midX,
-      y: this.metaData.midY,
-      dx: xDirection * this.metaData.initialSpeed,
-      dy: yDirection * this.metaData.initialSpeed,
-    };
   }
 }
