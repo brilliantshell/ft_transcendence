f420cf1768987ff015b1ac487a04c8b69098e01f
yongjulejule <lyjshow200@gmail.com>
2023-03-08 20:36:43 +0900

    :hammer: chore : 파일명 변경 및 사용하지 않는 모듈 삭제

--- frontend/src/views/Ranks.tsx
+++ frontend/src/views/Ranks.tsx
@@ -1,7 +1,7 @@
 // import MyRank from '../components/Rank/MyRank';
-import RanksList from '../components/Rank/RankList';
+import RanksList from '../components/Rank/RanksList';
 import { socket } from '../util/Socket';
-import { lazy, Suspense, useEffect, useState } from 'react';
+import { lazy, Suspense, useState } from 'react';
 import { useCurrentUi } from '../components/hooks/EmitCurrentUi';
 
 const MyRank = lazy(() => import('../components/Rank/MyRank'));
