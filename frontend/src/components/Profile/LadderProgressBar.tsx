ff3c313db6b50d695c5ad7b434fa8f369b739e9b
eunlee <eklee2057@naver.com>
2023-03-11 13:26:06 +0900

    ✨ feat : profile 페이지 레이아웃 #231 profile 페이지 레이아웃 및 내부 컴포넌트 구현

---
+++ frontend/src/components/Profile/LadderProgressBar.tsx
@@ -0,0 +1,16 @@
+import '../../style/Profile/LadderProgressBar.css';
+
+/* TODO - level의 최고값이 100일 것으로 예상했으나, 수정 필요할 듯 */
+function LadderProgressBar(props: { ladder: number | undefined }) {
+  return (
+    <div className="profileItem ladderBar">
+      <div>LEVEL {props.ladder}</div>
+      <div
+        className="ladderProportion"
+        style={{ width: `${props.ladder}%` }}
+      ></div>
+    </div>
+  );
+}
+
+export default LadderProgressBar;
